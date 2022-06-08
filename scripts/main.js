e9e373c44146ebe0eee1bd550905254989994c30
Jovolynn Gragasin <jovolynngragasin@gmail.com>
2022-06-07 18:47:01 -0700

    created arrays
    created starting arrays

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+const sign = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
+const tarot = ["6", "3", "2"];
+const reading = tarot[0] + ".svg";
+
